10395aa49cec8ed0af8a2384bbe8d585f7b05964
aesyns <122628859+aesyns@users.noreply.github.com>
2023-05-27 01:52:24 -0400

    Create pb2thingxD.lua

---
+++ pb2thingxD.lua
@@ -0,0 +1,60 @@
+--real ass jin script! touch V
+local EnableRelicTrading = false
+local POOPMODE = true --DONT DISABLE!
+
+
+if (EnableRelicTrading) then
+    game:GetService("Players").LocalPlayer.PlayerGui.TradeUpGUI.contract.Visible = true
+end
+
+--vv !!!DONT TOUCH REALLY IMPORTANT!!! vv
+local req = game:GetService("Players").LocalPlayer.PlayerGui.Troll
+local Player = game:GetService("Players").LocalPlayer
+local GC = getconnections or get_signal_cons
+if GC then
+    req.Enabled = true
+		for i,v in pairs(GC(Player.Idled)) do
+			if v["Disable"] then
+				v["Disable"](v)
+			elseif v["Disconnect"] then
+				v["Disconnect"](v)
+			end
+		end
+	else
+		Players.LocalPlayer.Idled:Connect(function()
+			local VirtualUser = game:GetService("VirtualUser")
+			VirtualUser:CaptureController()
+			VirtualUser:ClickButton2(Vector2.new())
+		end)
+	end
+local GodBrick = Instance.new("Part")
+GodBrick.Parent = game.Workspace
+GodBrick.Anchored = true
+GodBrick.Transparency = 1
+GodBrick.CanCollide = false
+if not POOPMODE then game:GetService("Players").LocalPlayer:Kick("BANNED, DIDN'T USE POOP MODE! O_O") end
+--^^ !!!DONT TOUCH REALLY IMPORTANT!!! ^^
+
+while true do
+    task.wait(0.4)
+    for i, v in pairs(game.workspace.Game.Trinkets.Spawned:GetChildren()) do
+        if v:IsA("Part") or v:IsA("MeshPart") then
+            Player.Character.Head.Anchored = false
+            Player.Character.HumanoidRootPart.Anchored = false
+            GodBrick.CFrame = Player.Character.HumanoidRootPart.CFrame
+            task.wait(0.1)
+            Player.Character.HumanoidRootPart.CFrame = v.CFrame
+            task.wait(0.1)
+            Player.Character.HumanoidRootPart.Anchored = true
+                task.wait(0.1)
+            if (Player.PlayerGui.Artifact.main.Visible) then
+                firesignal(Player.PlayerGui.Artifact.main.yes.MouseButton1Down)
+            end
+            task.wait(0.1)
+            Player.Character.HumanoidRootPart.Anchored = false
+            task.wait(0.1)
+            Player.Character.HumanoidRootPart.CFrame = GodBrick.CFrame
+            task.wait(0.1)
+        end
+    end
+end
